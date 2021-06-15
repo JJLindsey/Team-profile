6f3f6182f6811095ea4497f93ed499653ab42f6e
JJLindsey <jlindsey010@gmail.com>
2021-06-15 15:41:45 -0500

    add initial files

---
+++ src/html.template.js
@@ -0,0 +1,28 @@
+const generateHTML = teamArray => {
+    return `
+        <section class="my-3" id="portfolio">
+            <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+            <div class="flex-row justify-space-between">
+                <!-- Leaving this empty as we'll dynamically insert project HTML here -->
+            </div>
+        </section>
+        `;
+};
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+        <link rel="stylesheet" href="/dist/style.css">
+    </head>
+    <body>
+        
+        <script src="index.js"></script>
+    </body>
+</html>
